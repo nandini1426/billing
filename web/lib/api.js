19951c42f85d959ff8ae935cd534ec69f1c08038
Anonymous11257 <anonymousresearchpaperase@gmail.com>
2026-04-21 14:42:51 +0530

    commit

--- web/lib/api.js
+++ web/lib/api.js
@@ -1,32 +1,20 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:4000/api',
-  withCredentials: true,
+  baseURL: process.env.NEXT_PUBLIC_API_URL || 'http://localhost:4000/api',
+  headers: { 'Content-Type': 'application/json' },
 });
 
-// Attach JWT token to every request
 api.interceptors.request.use((config) => {
-  if (typeof window !== 'undefined') {
-    const token = localStorage.getItem('token');
-    if (token) config.headers.Authorization = `Bearer ${token}`;
-  }
+  const token = typeof window !== 'undefined'
+    ? localStorage.getItem('token') : null;
+  if (token) config.headers.Authorization = `Bearer ${token}`;
   return config;
 });
 
-// Auto logout on 401
 api.interceptors.response.use(
   (res) => res,
-  (err) => {
-    if (err.response?.status === 401) {
-      if (typeof window !== 'undefined') {
-        localStorage.removeItem('token');
-        localStorage.removeItem('user');
-        window.location.href = '/login';
-      }
-    }
-    return Promise.reject(err.response?.data || err);
-  }
+  (err) => Promise.reject(err.response?.data || err)
 );
 
 export default api;
